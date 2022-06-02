fa943905e9639a6f9441853e2d05b9a59b12fbbf
Tenaw Derseh <mete439@gmail.com>
2022-06-01 18:48:31 -0700

    part of the job done

---
+++ fastfood.R
@@ -0,0 +1,45 @@
+
+install.packages("openintro")
+
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(openintro))
+
+fastfood <- openintro::fastfood
+
+Q1 <- fastfood%>%
+  group_by(item, restaurant)%>%
+  summarise(sum = sum(calories))%>%
+  filter(restaurant == "Burger King" | restaurant == "Chick Fil-A")%>%
+  arrange(desc(sum))%>%
+  head(1)%>%
+  select(item)
+
+
+Q2 <- fastfood%>%
+  group_by(restaurant)%>%
+  summarise(mean = round(mean(sugar), 2), .groups = 'drop')%>%
+  filter(restaurant == "Subway")%>%
+  select(mean)
+
+
+Q3 <- fastfood%>%
+  group_by(restaurant)%>%
+  summarise(mean = mean(calories))%>%
+  filter(restaurant == "Taco Bell")%>%
+  select(mean)
+
+
+Q4 <- fastfood%>%
+  mutate(fatXsugar = total_fat*sugar)%>%
+  select(restaurant,item, fatXsugar)%>%
+  arrange(desc(fatXsugar))%>%
+  head(3)
+
+
+Q5 <- fastfood%>%
+  group_by(restaurant)%>%
+  summarise(mean = round(mean(sat_fat),2))%>%
+  filter( mean > 10)%>%
+  summarise(total = n())
+
+
